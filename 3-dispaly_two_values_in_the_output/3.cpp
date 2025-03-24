9c41d2adfb0a2cdc8b582fd100973b7af5ade545
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-25 00:50:54 +0330

    3-display

---
+++ 3-dispaly_two_values_in_the_output/3.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	x = 162;
+	y = 38;
+	cout << "x=";
+	cout << x;
+	cout << ", y=";
+	cout << y;
+	return 0;
+}
+
+
+
