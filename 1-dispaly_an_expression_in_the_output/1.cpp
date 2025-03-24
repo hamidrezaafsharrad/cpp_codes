1ca18ee122eb1387af1a5d4ab5df99a1f69cbd19
afshargithub <afsharrad__eedu@yahoo.com>
2025-03-25 00:07:00 +0330

    1-dispaly

---
+++ 1-dispaly_an_expression_in_the_output/1.cpp
@@ -0,0 +1,15 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello, This is a C++ Example.";
+	return 0;
+}
+
