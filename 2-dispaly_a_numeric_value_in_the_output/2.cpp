89d698f731f9e03ef4e40404e3409139a9ded2ac
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-25 00:23:07 +0330

    2-dispaly

---
+++ 2-dispaly_a_numeric_value_in_the_output/2.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	x = 162;
+	cout << x;
+	return 0;
+}
+
