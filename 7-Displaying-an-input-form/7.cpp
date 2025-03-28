016eb6ed5fb0eeeee1fa93c0e6b54394e2d1b0c2
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:05:06 +0330

    Displaying an input form

---
+++ 7-Displaying-an-input-form/7.cpp
@@ -0,0 +1,21 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "    First Name :\t\t                         Last Name :" << endl;
+	cout << "     ID Number : \t\t                         Phone NO. :" << endl;
+	cout << "Marital Status : \t\t                        Birth Date :" << endl;
+	cout << "  Passport NO. : \t\t                   BS/MS/Doctorate :" << endl;
+	cout << "        Salary :" << endl;
+	cout << "       Address :" << endl;
+}
+
+
+
