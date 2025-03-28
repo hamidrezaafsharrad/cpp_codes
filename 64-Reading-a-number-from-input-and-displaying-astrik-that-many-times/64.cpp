05685517a3ceaf54cf320f81ad0aee25d920083b
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:11:32 +0330

    Reading a number from input and displaying astrik that many times

---
+++ 64-Reading-a-number-from-input-and-displaying-astrik-that-many-times/64.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, sc;
+	cout << "Enter a number : ";
+	cin >> sc;
+	cout << endl << sc << '\t';
+	for(i=1; i <= sc; i++)
+		cout << '*';
+}
+
