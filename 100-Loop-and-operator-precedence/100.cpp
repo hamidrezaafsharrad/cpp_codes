49a27902ba17a54e2cb88f7045398429a1ab6122
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:09:43 +0330

    Loop and operator precedence

---
+++ 100-Loop-and-operator-precedence/100.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 10;
+	do {
+		cout << (--i)-- << " ";
+	} while (!((i >= 2) && (i < 5)));
+}
+
