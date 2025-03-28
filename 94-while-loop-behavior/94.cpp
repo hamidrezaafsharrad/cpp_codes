b60cf5d2b2d903bd9d7ace4c3f30d85037108511
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:01 +0330

    while loop behavior

---
+++ 94-while-loop-behavior/94.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float i = 10;
+	while (i < 10)
+	{
+		cout << 1 / i << endl;
+		i++;
+	}
+}
+
