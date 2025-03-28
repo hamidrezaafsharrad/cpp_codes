5caa910c74089794cbf3cd5d18c684cc9010efe9
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:41 +0330

    Counting for loop iterations

---
+++ 81-Counting-for-loop-iterations/81.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ 	int i;
+	for (i = 0; i = 10; i++)	
+		cout << i;
+}
+
