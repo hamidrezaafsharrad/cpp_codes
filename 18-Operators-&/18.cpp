ab971d6208e536bd737ff2a1553a15439a392789
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:14:22 +0330

    Operators &

---
+++ 18-Operators-&/18.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned char a = 0b10101010;
+	unsigned char b = 0b01010101;
+	cout << "1) a & b : " << (int)(a&b) << endl;
+	cout << "2) a | b : " << (int)(a|b) << endl;
+	cout << "4) a ^ b : " << (int)(a^b) << endl;
+}
+
+
