9081b973ee349d41c0c01d54c7415c7d97b81f3f
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:09:31 +0330

    while loop and break

---
+++ 104-while-loop-and-break/104.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n=0;
+	while (n < 5)
+	{
+		if (n > 3) break;
+		n++;
+	}
+	
+	//--- display value of n ---
+	cout << n;
+}
+
