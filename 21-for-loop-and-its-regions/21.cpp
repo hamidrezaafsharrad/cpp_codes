748f8fb83bcded174ab0b2e2f4163f8298168f49
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:22:33 +0330

    for loop and its regions

---
+++ 21-for-loop-and-its-regions/21.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int sum, k=0;
+	for (k=-100;  k < 1; sum=0, k=100)
+	cout << "k = " << k << endl;
+	k--;
+}
+
+
+
