6791960d3878191e61a6c0ca048b9be9e0ea7b2b
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:47 +0330

    Infinite for loop

---
+++ 23-Infinite-for-loop/23.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int k;
+	for (k=0;  k < 50; k=1)
+	{
+		cout << "salam";
+	}
+		cout << "k = " << k << endl;
+}
+
+
+
+
