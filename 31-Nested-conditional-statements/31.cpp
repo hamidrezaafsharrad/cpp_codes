404da23105cfd636052b50ff5919182f5965ba56
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:22 +0330

    Nested conditional statements

---
+++ 31-Nested-conditional-statements/31.cpp
@@ -0,0 +1,25 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	if (a == b)
+	if (b == c)
+	if (c == d)
+	if (d == e)
+	if (e == f)
+		cout << "OK";
+	else
+		cout << "NO";
+	return 0;
+}
+
+
