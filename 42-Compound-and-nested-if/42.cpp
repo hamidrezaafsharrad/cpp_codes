4eb5ed2d75d95451b80fc0d45c504c14f13ac8fa
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:51 +0330

    Compound and nested if

---
+++ 42-Compound-and-nested-if/42.cpp
@@ -0,0 +1,26 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int m = 6;
+	int x = 8;
+	if (m == 6)
+	{
+		if (x > 5 && x <10)
+			cout << x - m << endl;
+		else
+			cout << m + x << endl;
+	}
+	else if (m == 16)
+		cout << "tree" << endl;
+	else 
+		cout << "bye" << endl;
+}
+
