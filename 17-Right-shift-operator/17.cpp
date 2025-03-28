76d05fe3679dff596f1da3618a6ab5607f746dc3
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:13:45 +0330

    Right shift operator

---
+++ 17-Right-shift-operator/17.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned short int a = 0b1011010110101101;
+	cout << "befor shift : " << a << endl;
+	a >>= 1;
+	cout << "after shift : " << a << endl;
+}
+
+
