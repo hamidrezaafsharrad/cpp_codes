a0d1a9ba0f66c07a94635bfa89093d48c2520442
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:19 +0330

    Writing a program to compute 2 raised to a number

---
+++ 33-Writing-a-program-to-compute-2-raised-to-a-number/33.cpp
@@ -0,0 +1,21 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int res = 1, n, i;
+	cout << "Enter n : ";
+	cin >> n;
+	for (i=1; i <= n; i++)
+		res *= 2;
+	cout << "Result = " << res;
+}
+
+
+
