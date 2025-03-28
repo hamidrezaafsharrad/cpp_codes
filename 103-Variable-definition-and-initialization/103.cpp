12442ad766b247bb6e2bd8269dcc09782b339c03
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:09:34 +0330

    Variable definition and initialization

---
+++ 103-Variable-definition-and-initialization/103.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  const int x=7; x+9;
+  
+  //--- display value of x ---
+  cout << x;
+}
+
+
+
