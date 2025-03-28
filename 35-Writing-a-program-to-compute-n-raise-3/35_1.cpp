643c325d4028df5f172be4e8b6ab48261a385f36
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:15 +0330

    Writing a program to compute n raise 3

---
+++ 35-Writing-a-program-to-compute-n-raise-3/35_1.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter n : ";
+	cin >> n;
+	cout << "Result = " << n * n * n;
+}
+
---
+++ 35-Writing-a-program-to-compute-n-raise-3/35_2.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main()
+{
+	int n;
+	cout << "Enter n : ";
+	cin >> n;
+	cout << "Result = " << pow(n, 3);
+}
+
+
