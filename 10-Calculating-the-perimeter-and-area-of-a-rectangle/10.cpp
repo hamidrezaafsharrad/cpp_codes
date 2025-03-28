bd81f2b08c59fd9d522b398d1cd49c419ce6cb54
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:08:31 +0330

    Calculating the perimeter and area of a rectangle

---
+++ 10-Calculating-the-perimeter-and-area-of-a-rectangle/10.cpp
@@ -0,0 +1,21 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, Mohit, Masahat;
+	cin >> a >> b;
+	Mohit = 2 * (a + b);
+	Masahat = a * b;
+	cout << "Mohit= " << Mohit << endl;
+	cout << "Masahat= " << Masahat << endl;
+	return 0;
+}
+
+
