977d8efca3d163602242ee5563681cbc31c068a8
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:06 +0330

    Displaying output with if...else

---
+++ 38-Displaying-output-with-if...else/38.cpp
@@ -0,0 +1,25 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int k=0, y=1;
+	string s = "book";
+	if ( !k )
+	{
+		cin >> s;
+		cout << y;
+	}
+	cout << endl;
+	if ( y )
+		cout << y + k;
+	else
+		cout << k + y;
+}
+
