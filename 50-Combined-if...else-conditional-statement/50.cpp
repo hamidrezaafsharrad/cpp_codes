010bc0af4a589888b7da61cc52389b697c7e5e6c
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:15 +0330

    Combined if...else conditional statement

---
+++ 50-Combined-if...else-conditional-statement/50.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int b = 20;
+	if (b > 50 && b < 80 && b != 60)
+		cout << "AA";
+	else
+		cout << "BB";
+}
+
