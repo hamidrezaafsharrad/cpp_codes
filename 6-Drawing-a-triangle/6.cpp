80451d6de52a3facab895f2a37a0452a8df70b50
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 11:59:15 +0330

    Drawing-a-triangle

---
+++ 6-Drawing-a-triangle/6.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "*******" << endl;
+	cout << "******" << endl;
+	cout << "*****" << endl;
+	cout << "****" << endl;
+	cout << "***" << endl;
+	cout << "**" << endl;
+	cout << "*";
+	return 0;
+}
+
+
+
