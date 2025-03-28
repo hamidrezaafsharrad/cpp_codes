080162e6116c752c2d2b2fbb7ceadea277232e4a
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:38 +0330

    Sequential if statements without else

---
+++ 45-Sequential-if-statements-without-else/45.cpp
@@ -0,0 +1,25 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int b=200;
+	if (b > 1)
+		cout << "A1";
+	if (b > 10)
+		cout << "A10";
+	if (b > 100)
+		cout << "A100";
+	if (b > 1000)
+		cout << "A1000";
+	if (b > 10000)
+		cout << "A10000";
+}
+
+
