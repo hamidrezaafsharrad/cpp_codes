477afa26321e630345f39e37402f282346eb1a65
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:51 +0330

    Displaying output using a loop

---
+++ 78-Displaying-output-using-a-loop/78.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 1;
+	while (i < 10)
+	{
+		cout << 'A' << endl;
+		i++;
+	}
+}
+
