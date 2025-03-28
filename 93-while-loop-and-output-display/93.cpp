2ef9e35c2b0b75388d3f7d1ad775920a6bcbdcda
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:04 +0330

    while loop and output display

---
+++ 93-while-loop-and-output-display/93.cpp
@@ -0,0 +1,21 @@
+	/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int b=0, c=70;
+	while (b >= 10 || c > 50)
+	{
+		cout << 'A';
+		c -= 5;
+		b += 4;
+	}
+}
+
