7003f36d6e6f3fe12cdee127fed1fd1eabdca6b2
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:15:01 +0330

    Summation and while loop

---
+++ 19-Summation-and-while-loop/19.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n=1, sum=0;
+	while (n <= 70)
+	{
+		sum += n;
+		++n;
+	}
+	cout << "1+2+3+4+5+...+69+70 = " << sum;
+	return 0;
+}
+
+
+
