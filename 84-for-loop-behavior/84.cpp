e718a9af29640c9c61d4a461c333fc568e513dae
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:32 +0330

    for loop behavior

---
+++ 84-for-loop-behavior/84.cpp
@@ -0,0 +1,16 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int m = 95, n = 11; m % n > 2; m -= 3, n++)
+		cout << m % n;
+}
+
