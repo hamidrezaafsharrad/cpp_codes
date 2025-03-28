7537f8a7e6c0ddb75f6cbf83e308bf7ec6526df3
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:09:43 +0330

    Determining if a number is odd or even

---
+++ 11-Determining-if-a-number-is-odd-or-even/11.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "Enter a Number : ";
+	cin >> x;
+	if (x%2 == 0)
+		cout << "zoj";
+	else
+		cout << "fard";
+	return 0;
+}
+
+
