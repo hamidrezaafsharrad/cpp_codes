ad635866cb57e5408a9edb8143f74b6979e31c3b
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:00 +0330

    Initializing array elements during declaration and displaying in reverse

---
+++ 55-Initializing-array-elements-during-declaration-and-displaying-in-reverse/55.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int i;
+	char harf[7] = {'R', 'E', 'D', 'B', 'O', 'O', 'K'};
+	for(i=6; i > 2; i--)
+		cout << harf[i] << endl;
+	return 0;
+}
+
