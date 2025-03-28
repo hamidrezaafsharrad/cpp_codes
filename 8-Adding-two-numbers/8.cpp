8df0eef01977d379bbec6a96d53004123f5da9e2
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:05:55 +0330

    Adding two numbers

---
+++ 8-Adding-two-numbers/8.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main() {
+	int a; 
+	int b; 
+	int sum; 
+	cout << "Enter first integer number: ";
+	cin >> a; 
+	cout << "Enter second integer number: ";
+	cin >> b;
+	sum = a + b;
+	cout << sum;
+	return 0;
+}
+
