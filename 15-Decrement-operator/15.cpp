93791eb069e8f525304772d28ca13dee7b5e274f
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:12:11 +0330

    Decrement operator

---
+++ 15-Decrement-operator/15.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 162 , b = -1;
+	a += (b*2) - a;
+	a--;
+	a -= -3;
+	cout << b;
+}
+
+
