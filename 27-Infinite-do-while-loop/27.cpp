f829f8b6fe8df809aca37e74ca0464ee5e191db6
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:36 +0330

    Infinite do while loop

---
+++ 27-Infinite-do-while-loop/27.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, k;
+	k = 162;
+	do{
+		i = 8;
+		i++;
+		for (k=i; i == 9; i++)
+			cout << k;
+		i--;
+	}while(i = 1);
+}
+
