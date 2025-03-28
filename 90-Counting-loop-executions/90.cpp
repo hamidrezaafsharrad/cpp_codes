7cf4b94005111b3ad2a933f858f460f8381636d4
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:14 +0330

    Counting loop executions

---
+++ 90-Counting-loop-executions/90.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int b = 20;
+	while (b < 20)
+		cout << 9;
+}
+
