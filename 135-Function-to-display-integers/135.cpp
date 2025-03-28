a479133b3acba0bf1d7eb959f61dea3453a5239c
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:08:46 +0330

    Function to display integers

---
+++ 135-Function-to-display-integers/135.cpp
@@ -0,0 +1,24 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+//---function definitions---
+void DisplayOddNumbers_1_20()
+{
+	int i;
+	for(i=1; i <= 20; i+=2)
+		cout << i << endl;
+}
+
+int main() 
+{
+	//---function call---
+	DisplayOddNumbers_1_20(); 
+}
+
