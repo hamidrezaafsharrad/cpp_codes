41557f478142f6c38ee87261b98c510100df8e50
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:09:58 +0330

    Loop and calculating a number series

---
+++ 95-Loop-and-calculating-a-number-series/95.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double i = 1;
+	while (i > 0 && i < 10)
+	{
+		cout << 1 / i << endl;
+		i++;
+	}
+}
+
