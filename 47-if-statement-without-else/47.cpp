66bac7a137874ff061276423e137346bb3df8dcd
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:30 +0330

    if statement without else

---
+++ 47-if-statement-without-else/47.cpp
@@ -0,0 +1,21 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int b;
+	cin >> b;
+	if (b > 1)
+		if (b > 10)
+			if (b > 100)
+				if (b > 1000)
+					if (b == 10000)
+						cout << "OK";
+}
+
