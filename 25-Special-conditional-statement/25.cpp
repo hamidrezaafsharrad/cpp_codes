94b2dece497047f3e2d219dd28d43fde630118fe
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:39 +0330

    Special conditional statement

---
+++ 25-Special-conditional-statement/25.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	if ( m < n );
+		cout << m + n;
+}
+
+
