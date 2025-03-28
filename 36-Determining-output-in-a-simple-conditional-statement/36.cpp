7f4836c3fe1247221aa0567fb602427ab9431b91
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:12 +0330

    Determining output in a simple conditional statement

---
+++ 36-Determining-output-in-a-simple-conditional-statement/36.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	if (x== 50) {
+		cout << "x is ";
+		cout << x;
+	}
+}
+
+
