155517da2c2e926b71e2c40cb58c5ee564b5385f
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:33 +0330

    Running a loop until a specific keyboard input is read

---
+++ 28-Running-a-loop-until-a-specific-keyboard-input-is-read/28.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	char k;
+	cin >> k;
+	while ( !(k == 'Q'  || k == 'q') )
+	{
+		cout << "vorodi = " << k << endl;
+		cin >> k;
+	}
+}
+
+
+
