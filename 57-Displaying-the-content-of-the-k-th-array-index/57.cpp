809ab8b055a8bf970d25cff1f397e7d5b20c3260
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:11:54 +0330

    Displaying the content of the k th array index

---
+++ 57-Displaying-the-content-of-the-k-th-array-index/57.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main() 
+{
+	char harf[7] = {'R', 'E', 'D', 'B', 'O', 'O', 'K'};
+	int k = sizeof(harf); //length of array ie k=7
+	k %= 2;
+	cout << harf[k] << endl;
+	return 0;
+}
+
