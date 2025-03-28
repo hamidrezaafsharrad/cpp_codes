1885e34cdfbff4490bc007315270f3d5a3cfbd71
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:02 +0330

    Displaying with if without else

---
+++ 39-Displaying-with-if-without-else/39.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x=1, y=0;
+	string s = "book";
+	if ( !(x > y) )
+			cout << "hello" << endl;
+			cout << "bye" << endl << s;
+}
+
