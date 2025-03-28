395ea053397412411b6eb1bbe49e5944f2be2d4a
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:34 +0330

    Nested if without else

---
+++ 46-Nested-if-without-else/46.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int b=200;
+	if (b > 1)
+	if (b > 10)
+		cout << "A10";
+}
+
