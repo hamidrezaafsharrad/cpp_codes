de99f0cf634b758e35060ad7081ab819df83d7eb
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:42 +0330

    Infinite loop

---
+++ 24-Infinite-loop/24.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int j;
+	for (j=10;  j > 0; ) cout << "nop" << endl;
+		j--;
+		cout << j << endl;
+}
+
+
