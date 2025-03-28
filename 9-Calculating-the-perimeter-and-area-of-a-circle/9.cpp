aa95a85281f036ce9e5e1f9960f14bf364a55bbe
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:07:04 +0330

    Calculating-the-perimeter-and-area-of-a-circle

---
+++ 9-Calculating-the-perimeter-and-area-of-a-circle/9.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int r;
+	float PI = 3.14, Mohit, Masahat;
+	cin >> r;
+	Mohit = 2 * PI * r;
+	Masahat = PI * r * r;
+	cout << "Mohit= " << Mohit << endl;
+	cout << "Masahat= " << Masahat << endl;
+	return 0;
+}
+
+
