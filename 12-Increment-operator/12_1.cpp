1da3f021dadc50dd80ef82487c3149e424eb5be2
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:10:17 +0330

    Increment operator

---
+++ 12-Increment-operator/12_1.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	a = 10;
+	a++;
+	++a;
+	cout << a;
+}
+
+
---
+++ 12-Increment-operator/12_2.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	a = 10;
+	b = a++;
+	cout << b;
+	cout << endl;
+
+	a = 10;
+	c = ++a;
+	cout << c;
+}
+
+
