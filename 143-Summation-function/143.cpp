94f25db7df88e8bfbada238ea9ce50f0941017e6
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:08:22 +0330

    Summation function

---
+++ 143-Summation-function/143.cpp
@@ -0,0 +1,24 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+//---function definitions---
+float MyAdd(float num1, float num2)
+{
+	return num1 + num2;
+}
+
+int main() 
+{
+	//---function call---
+	cout << "12 +  7 : " << MyAdd(12, 7)  << endl; 
+	cout << "-2 +  3 : " << MyAdd(-2, 3)  << endl; 
+	cout << "-5 + -2 : " << MyAdd(-5, -2) << endl; 
+}
+
