104318ba4d03aecd591ce934657bfce2f0cb8c57
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:11:10 +0330

    Detecting English vowels

---
+++ 13-Detecting-English-vowels/13.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout << "Enter a character: ";
+	cin >> ch;
+	if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || 
+	    ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')  
+	    cout << "YES";
+	else
+	    cout << "NO";
+}
+
+
