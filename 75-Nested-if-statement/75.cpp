4904df1728bcb4ed8e6a91c0395ca0de67271034
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:11:01 +0330

    Nested if statement

---
+++ 75-Nested-if-statement/75.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main() {
+	int a=0, b=1, c=2;
+	
+	if (a == 1)
+	if (b == 1) c++;
+	else c--;
+	
+	cout << c;
+}
+
+
