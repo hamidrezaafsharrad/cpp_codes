61f9205079cd50368ad96dde51d66d4e2f4f4251
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:57 +0330

    Short conditional statement

---
+++ 76-Short-conditional-statement/76.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main() {
+	int x=2, y=3, p;
+	
+	p = (x > y ? 4 : 5);
+	
+	cout << p;
+}
+
