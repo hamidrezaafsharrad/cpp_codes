0e4feee55489afabc8cbc09491b0ac93afd80eda
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:23 +0330

    Estimating loop iteration count

---
+++ 87-Estimating-loop-iteration-count/87.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool i = false;
+	do {
+		i = true;
+	} while (!i);
+}
+
