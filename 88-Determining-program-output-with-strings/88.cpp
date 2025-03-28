6a1a08d561e35b1e9d11f43b2628bd3d62064ed3
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:20 +0330

    Determining program output with strings

---
+++ 88-Determining-program-output-with-strings/88.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s1 = "ABCDEFG";
+	string s2 = "HKM";
+	string s3 = s1 + "LL";
+	s2 += s3;
+	cout << s2;
+}
+
