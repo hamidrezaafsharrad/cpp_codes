6f8b14f373e1269bc3ee76c7815b4f0eeb41b776
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:09 +0330

    Direct assignment to array elements

---
+++ 52-Direct-assignment-to-array-elements/52.cpp
@@ -0,0 +1,25 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	float Nomre[6];
+	Nomre[0] = 18.5;
+	Nomre[1] = 15.25;
+	Nomre[2] = 19.75;
+	Nomre[3] = 16;
+	Nomre[4] = 15;
+	Nomre[5] = 12;
+	cout << Nomre[0] << endl;
+	cout << Nomre[2] << endl;
+	cout << Nomre[4] << endl;
+}
+
+
