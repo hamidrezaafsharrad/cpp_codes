3974bab0be7b9b05d7314aa798b0e3f3654ca1dc
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 12:13:16 +0330

    Left shift operator

---
+++ 16-Left-shift-operator/16.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a = 0b00011011;
+	cout << "befor shift : " << (int)a << endl;
+	a <<= 1;
+	cout << "after shift : " << (int)a << endl;
+}
+
+
+
