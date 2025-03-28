1e1790825f93eb354e2f607f052109cb057037db
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:10:11 +0330

    Counting while loop iterations

---
+++ 91-Counting-while-loop-iterations/91.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int b = 0;
+	while (b < 10)
+	{
+		cout << 'A';
+		b += 2;
+	}
+}
+
