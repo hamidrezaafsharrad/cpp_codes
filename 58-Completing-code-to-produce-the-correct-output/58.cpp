d7e70620104ed2996fb49d6b6978055373eb4f3b
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:11:51 +0330

    Completing code to produce the correct output

---
+++ 58-Completing-code-to-produce-the-correct-output/58.cpp
@@ -0,0 +1,28 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int i;
+	int X[7]={0};
+	
+	X[2] = 110;
+	X[3] = 162;
+	X[5] = 313;
+	X[6] = 137;
+	
+	cout << X[3] << endl << endl;
+	cout << X[5] << endl << endl;
+	cout << X[3] << endl << endl;
+	cout << X[6] << endl << endl;
+	cout << X[2] << endl;
+	return 0;
+}
+
+
