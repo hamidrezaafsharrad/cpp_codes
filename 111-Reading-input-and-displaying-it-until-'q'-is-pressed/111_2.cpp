09a3f62671c082425cc65d38e19180c5859f3524
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:09:08 +0330

    Reading input and displaying it until 'q' is pressed

---
+++ 111-Reading-input-and-displaying-it-until-'q'-is-pressed/111_2.cpp
@@ -0,0 +1,35 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	char ch;
+	string str="";//empty string definition
+	const int str_len=100;
+	int count=0;
+	
+	cout << "Enter text until reach to 'Q' or 'q' then display all text" << endl;
+	do{
+		ch = getche();
+		count++;
+		
+		if (ch!='q' && ch!='Q' && count<=str_len)
+			str += ch;
+		else
+			break;
+	}while(true);
+	
+	if (str == "")
+		cout << endl << "nothing";
+	else
+		cout << endl << str;
+}
+
