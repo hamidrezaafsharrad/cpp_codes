7c8c1a0a9dd562bb9d6347635a95c43d5c153569
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:08:34 +0330

    Square root function

---
+++ 139-Square-root-function/139.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+	//---function call---
+	cout << "square root 4 : " << sqrt(4) << endl; 
+	cout << "square root 2 : " << sqrt(2) << endl; 
+	cout << "square root 8 : " << sqrt(8) << endl; 
+}
+
