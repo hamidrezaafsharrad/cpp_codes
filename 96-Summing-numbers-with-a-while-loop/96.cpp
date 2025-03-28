a26911bf6f8ab341d76a18451fe80761d9d7445f
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:09:55 +0330

    Summing numbers with a while loop

---
+++ 96-Summing-numbers-with-a-while-loop/96.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, sum=0;
+	cin >> a;
+	while (a != 0)
+	{
+		sum += a;
+		cin >> a;
+	}
+	cout << "--->" << sum << endl;
+}
+
