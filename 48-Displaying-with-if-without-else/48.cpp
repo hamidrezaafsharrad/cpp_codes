7fd3896c45a3db2891c17b8411c3cd8b859c4c4e
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:21 +0330

    Displaying with if without else

---
+++ 48-Displaying-with-if-without-else/48.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int b;
+	cin >> b;
+	if (b > 5)
+		if (b < 8)
+			if (b != 6)
+				cout << "OK";
+}
+
