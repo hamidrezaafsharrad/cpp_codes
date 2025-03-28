22834283c4048517833308f3069adfc2a1cd4672
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:12 +0330

    Displaying with if...else

---
+++ 51-Displaying-with-if...else/51.cpp
@@ -0,0 +1,18 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int b = 20;
+	if (!(b == 60))
+		cout << "S1";
+	else
+		cout << "S15";
+}
+
