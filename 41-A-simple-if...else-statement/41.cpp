8f1dbbf590c557aa9c9470a14a53e3395d5bc1a9
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:12:55 +0330

    A simple if...else statement

---
+++ 41-A-simple-if...else-statement/41.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int m;
+	m = 7;
+	if (m == 6)
+	{
+		cout << "1347" << endl;
+	}				
+	else
+		cout << "162" << endl;
+}
