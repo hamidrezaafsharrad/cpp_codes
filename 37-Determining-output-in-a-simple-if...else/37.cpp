90d5491a507d1111447376ca6d7621cd534d6b93
hamidrezaafsharrad <hrafsharrad@gmail.com>
2025-03-28 13:13:09 +0330

    Determining output in a simple if...else

---
+++ 37-Determining-output-in-a-simple-if...else/37.cpp
@@ -0,0 +1,26 @@
+/*****************************************
+example, written by Hamidreza Afsharrad
+			  spring 2024
+				     1403
+			   good luck
+			  best regards
+*****************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cin >> x;
+	
+	if (x > 50)
+	{
+		cout << x;
+		cout << " is greater than 50";
+	}
+	else
+	{
+		cout << x;
+		cout << " is less than 50";
+	}
+}
+
